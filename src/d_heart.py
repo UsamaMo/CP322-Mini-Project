d917bae16b4c573cef0864c0345588b9dbf71dcb
akif5298 <134899543+akif5298@users.noreply.github.com>
2023-10-22 23:17:34 -0400

    Update d_heart.py

--- src/d_heart.py
+++ src/d_heart.py
@@ -1,7 +1,9 @@
 from ucimlrepo import fetch_ucirepo
 from collections import Counter
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+import ggpy as gg
 
 # Fetch dataset
 heart_disease = fetch_ucirepo(id=45)
@@ -13,55 +15,55 @@ y = heart_disease.data.targets.to_numpy()
 
 # Task 1
 def sigmoid(z):
-  return 1 / (1 + np.exp(-z))
+	return 1 / (1 + np.exp(-z))
 
 
 def logistic_regression_fit(X, y, learning_rate=0.01, num_iterations=1000):
-  m, n = X.shape
-  theta = np.zeros(
-      (n + 1, 1))  # Initialize theta, adding one for the intercept term
+	m, n = X.shape
+	theta = np.zeros(
+			(n + 1, 1))  # Initialize theta, adding one for the intercept term
 
-  # Add a column of ones to X for the intercept term
-  X = np.column_stack((np.ones(m), X))
+	# Add a column of ones to X for the intercept term
+	X = np.column_stack((np.ones(m), X))
 
-  for _ in range(num_iterations):
-    z = np.dot(X, theta)
-    h = sigmoid(z)
-    gradient = np.dot(X.T, (h - y)) / m
-    theta -= learning_rate * gradient
+	for _ in range(num_iterations):
+		z = np.dot(X, theta)
+		h = sigmoid(z)
+		gradient = np.dot(X.T, (h - y)) / m
+		theta -= learning_rate * gradient
 
-  return theta
+	return theta
 
 
 def logistic_regression_predict(X, theta):
-  # Add a column of ones to X for the intercept term
-  X = np.column_stack((np.ones(X.shape[0]), X))
-  z = np.dot(X, theta)
-  h = sigmoid(z)
-  y_pred = (h >= 0.5).astype(int)
-  return y_pred
+	# Add a column of ones to X for the intercept term
+	X = np.column_stack((np.ones(X.shape[0]), X))
+	z = np.dot(X, theta)
+	h = sigmoid(z)
+	y_pred = (h >= 0.5).astype(int)
+	return y_pred
 
 
 # Task 2
 def knn_predict(X_train, y_train, X, k=3):
-  y_pred = [knn_predict_single(x, X_train, y_train, k) for x in X]
-  return np.array(y_pred)
+	y_pred = [knn_predict_single(x, X_train, y_train, k) for x in X]
+	return np.array(y_pred)
 
 
 def knn_predict_single(x, X_train, y_train, k=3):
-  distances = [np.linalg.norm(x - x_train) for x_train in X_train]
-  k_indices = np.argsort(distances)[:k]
-  k_nearest_labels = [tuple(y_train[i])
-                      for i in k_indices]  # Convert y_train to a tuple
-  most_common = Counter(k_nearest_labels).most_common(1)
-  return most_common[0][0]
+	distances = [np.linalg.norm(x - x_train) for x_train in X_train]
+	k_indices = np.argsort(distances)[:k]
+	k_nearest_labels = [tuple(y_train[i])
+											for i in k_indices]  # Convert y_train to a tuple
+	most_common = Counter(k_nearest_labels).most_common(1)
+	return most_common[0][0]
 
 
 # Task 3
 def evaluate_acc(y_true, y_pred):
-  correct = np.sum(y_true == y_pred)
-  accuracy = correct / len(y_true)
-  return accuracy
+	correct = np.sum(y_true == y_pred)
+	accuracy = correct / len(y_true)
+	return accuracy
 
 
 # Testing
@@ -79,7 +81,7 @@ y_test = y[split_index:]
 learning_rate = 0.01
 num_iterations = 1000
 theta = logistic_regression_fit(X_train, y_train, learning_rate,
-                                num_iterations)
+																num_iterations)
 y_pred_logistic = logistic_regression_predict(X_test, theta)
 
 # Test K-Nearest Neighbor
@@ -90,5 +92,16 @@ y_pred_knn = knn_predict(X_train, y_train, X_test, k)
 accuracy_logistic = evaluate_acc(y_test, y_pred_logistic)
 accuracy_knn = evaluate_acc(y_test, y_pred_knn)
 
-print("Accuracy (Logistic Regression):", accuracy_logistic)
-print("Accuracy (K-Nearest Neighbor):", accuracy_knn)
+# Create a pandas DataFrame for results
+results = pd.DataFrame({
+		'Model': ['Logistic Regression', 'K-Nearest Neighbor'],
+		'Accuracy': [accuracy_logistic, accuracy_knn]
+})
+
+# Create a bar plot
+plt.figure(figsize=(8, 6))
+plt.bar(results['Model'], results['Accuracy'], color='lightblue')
+plt.ylim(0, 1)
+plt.title('Model Accuracy Comparison')
+plt.ylabel('Accuracy')
+plt.show()
